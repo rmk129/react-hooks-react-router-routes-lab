bf1a81d8923ff2d30d28328ac437455d1161ddb1
Romil <romil.m.kothari@gmail.com>
2023-06-05 12:08:05 -0400

    tests completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+ const displayedDirectors = directors.map((director)=> {
+
+  
+  
+  return (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>{director.movies.map((movie)=> (<li key={movie}>{movie}</li>))}</ul>
+    </div>
+  )
+
+
+ })
+
+  return (<div>
+    <h1>Directors Page</h1>
+    {displayedDirectors}
+   
+
+  </div>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+const displayedMovies = movies.map((movie)=> {
+  const movieGenres = movie.genres.map((genre)=> <li key={genre}>{genre}</li>)
+
+  return <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <h2>{movie.time}</h2>
+      <ul>{movieGenres}</ul>
+  </div>
+})
+
+  return <div>
+    <h1>Movies Page</h1>
+    {displayedMovies}
+  </div>;
 }
 
 export default Movies;
